5c79487a08b09aaab63445b536bacc79018df97d
masamoor <cyf1252251225@gmail.com>
2025-10-15 13:45:33 +0800

    init commit

---
+++ tree.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int num;
+    scanf("%d",&num);
+
+    for(int i=1;i <= num;i++){
+
+        for(int a=1;a <= num - i;a++){
+            printf(" ");
+        }
+        
+        for(int b=1;b <= i * 2 - 1;b++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
