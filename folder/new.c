d56034b0802a1d5617fbbd27407a9e58a60f425b
masamoor <cyf1252251225@gmail.com>
2025-10-21 15:29:45 +0800

    251021

---
+++ folder/new.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int item,ratio;
+    int sum=0;
+    int fir;
+    scanf("%d%d%d",&fir,&item,&ratio);
+
+    sum = fir;
+
+    for(int i=1;i <= item - 1;i++)
+    {
+        fir *= ratio;
+        sum += fir;
+    }
+
+    printf("%d",sum);
+    return 0;
+}
